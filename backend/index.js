c8537827a58a5b873f173cbe422b7e5710947e97
Yohel <perezyohel@gmail.com>
2020-11-03 22:57:06 -0500

    desarrolo del backend

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-//codigo para configurar el servidor cuando empieza 
-//comando en consola para ejecutar el servidor: node server/index.js
-const express= require('express'); //modulo de express
-const app = express();      //app se encarga de tener toda la funcionalidad del servidor
-app.listen(3000, ()=>{
-    console.log('server on port 3000');
-});
